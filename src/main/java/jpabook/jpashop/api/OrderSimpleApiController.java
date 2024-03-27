c22ab5305eb05480d74e1ec5c539b2d4da5db988
Tanguri <jungwontwo2@naver.com>
2024-03-27 14:47:49 +0900

    간단한 주문 조회 V1 엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 컬렉션이 아닌 xToOne(ManyToOne,OneToOne)
+ * Order를 조회하고
+ * Order->Member ManyToOne 관계
+ * Order->Delivery OneToOne 관계
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
